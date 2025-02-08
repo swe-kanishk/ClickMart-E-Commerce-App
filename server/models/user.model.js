d843fbf8e9597d34bbec38ce7ce516dead132ef5
kanishk tiwari <kanishk.swe@gmail.com>
2025-02-08 12:50:07 +0530

    user model done, server

---
+++ server/models/user.model.js
@@ -0,0 +1,79 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Provide FirstName!"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Provide LastName!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Provide Email!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Provide Password!"],
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    mobile: {
+      type: Number,
+      default: null,
+    },
+    verify_email: {
+      type: Boolean,
+      default: false,
+    },
+    last_login_date: {
+      type: Date,
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Inactive", "Suspended"],
+      default: "Active",
+    },
+    address_details: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "address",
+      },
+    ],
+    shopping_cart: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "cartProduct",
+      },
+    ],
+    orderHistory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "order",
+      },
+    ],
+    forgot_password_otp: {
+      type: String,
+      default: null,
+    },
+    forgot_password_otp_expiry: {
+      type: Date,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = model("User", userSchema);
+export default userModel;
