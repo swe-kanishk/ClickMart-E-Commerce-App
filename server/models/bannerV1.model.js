0a2a09d47e4b066ad7c52569adf5ea81bc555e3e
kanishk tiwari <kanishk.swe@gmail.com>
2025-03-23 16:01:29 +0530

    feat(server):banner v1 model done_

---
+++ server/models/bannerV1.model.js
@@ -0,0 +1,35 @@
+import {model, Schema} from 'mongoose';
+
+const bannerV1Schema = new Schema({
+  title: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  catId: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  subCatId: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  thirdLevelSubCatId: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+}, {timestamps: true});
+
+const BannerV1Model = model('BannerV1', bannerV1Schema);
+export default BannerV1Model;
