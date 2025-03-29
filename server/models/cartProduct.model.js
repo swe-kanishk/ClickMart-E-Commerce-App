c1edcbf76b5a77b9a0cbd7256d87334ae75ca7ef
kanishk tiwari <kanishk.swe@gmail.com>
2025-03-29 23:21:39 +0530

    cartProduct model done, server

--- server/models/cartProduct.model.js
+++ server/models/cartProduct.model.js
@@ -1,22 +1,55 @@
 import { model, Schema } from "mongoose";
 
-const cartProductSchema = new Schema(
-  {
-    productId: {
-      type: Schema.ObjectId,
-      ref: "Product",
-    },
-    quantity: {
-      type: Number,
-      default: 1,
-    },
-    userId: {
-      type: Schema.ObjectId,
-      ref: "User",
-    },
+const cartProductSchema = new Schema({
+  productId: {
+    type: String,
+    required: [true, "Product Id required!"],
   },
-  { timestamps: true }
-);
+  userId: {
+    type: String,
+    required: [true, "User Id required!"],
+  },
+  productTitle: {
+    type: String,
+    required: [true, "Product title required!"],
+  },
+  image: {
+    type: String,
+    required: [true, "Product image required!"],
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Product quantity required!"],
+  },
+  subTotal: {
+    type: Number,
+    required: [true, "subtotal required!"],
+  },
+  countInStock: {
+    type: Number,
+    required: [true, "Product stock count required!"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "Product rating required!"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price required!"],
+  },
+  oldPrice: {
+    type: Number,
+    required: [true, "Product old price required!"],
+  },
+  brand: {
+    type: String,
+    required: [true, "Product brand required!"],
+  },
+  discount: {
+    type: Number,
+    required: [true, "Product discount required!"],
+  },
+  }, { timestamps: true });
 
-const CartProductModel = model('CartProduct', cartProductSchema);
+const CartProductModel = model("CartProduct", cartProductSchema);
 export default CartProductModel;
